24ea79accf011c13c25de2a5df014822554916b7
Alok Shandilya <alok.shandilya10@gmail.com>
2024-02-05 23:47:51 +0530

    aliases, pyenv etc setup

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,1 +1,4 @@
 fnm env --use-on-cd | source
+# fnm
+set PATH "/home/aloks/.local/share/fnm" $PATH
+fnm env | source
--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,5 +1,6 @@
 function fish_title
-  set -q argv[1]; or set argv simple terminal, st : fish shell
+  # set -q argv[1]; or set argv simple terminal, st : fish shell
+  set -q argv[1]; or set argv Hey Alok, Welcome to ur ❤️ fish shell
   # Looks like ~/d/fish: git log
   # or /e/apt: fish
   echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
